0c78425453b80a250326a08496020fcbf4fc21f3
黄磊 <huanglei@citgc.com>
2019-11-21 14:53:32 +0800

    添加字体、图片、html、clean loader

--- 2-7loaders-start/src/index.js
+++ 2-7loaders-start/src/index.js
@@ -1,3 +1,21 @@
 require('./index.scss')
+import Logo from './logo.png'
+import Happy from './happy.jpg'
 
-console.log('hello webpack!')
+function addImg() {
+    const element = document.createElement('div');
+
+    // happy img
+    let happyImg = new Image();
+    happyImg.src = Happy
+    element.append(happyImg);
+
+    // logo img
+    let logoImg = new Image();
+    logoImg.src = Logo
+    element.append(logoImg);
+
+    return element;
+}
+
+document.body.appendChild(addImg());
--- 2-7loaders-start/webpack.config.js
+++ 2-7loaders-start/webpack.config.js
@@ -1,13 +1,24 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const config = {
   entry: './src/index.js',
+  mode: "development",
   output: {
     // path.join() 去拼接路径
     // __dirname 当前文件的绝对路径
     filename: 'bundle.js',
     path: path.join(__dirname, './dist')
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      name: 'index.html',
+      title: '2-7webpack loader相关作业'
+    })
+  ],
   module: {
     rules: [
       {
@@ -15,6 +26,36 @@ const config = {
         test: /\.(scss|sass)$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
       },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name]-[hash:4].[ext]',
+              limit: 5120,
+              outputPath: 'images/'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      }
     ]
   }
 }
